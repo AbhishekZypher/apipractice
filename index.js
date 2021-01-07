cc617ec025159d9fe6e072a5ac81910bf560a32a
abhishekZypher <abhishekthakur654@gmail.com>
2021-01-07 12:23:50 +0530

    second commitconsoeladded

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const app = express();
 mongoose.connect('mongodb://localhost/student', { useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB Server...'))
   .catch((err) => console.error('Something went wrong...'));
+console.log('heldsilfhsdhfsdhjfhdsjkh;');
 app.use('/api', route);
 app.listen(3000, () => console.log('Listen on port 3000'));
